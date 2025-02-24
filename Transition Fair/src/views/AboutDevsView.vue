a178b9cb9cb141213052c83ec24e86f132a3ad7f
MlzzRamdon <26rbutterfield@go.dsdmail.net>
2025-02-24 16:30:51 +0000

    Added dimensions to pictures

--- Transition Fair/src/views/AboutDevsView.vue
+++ Transition Fair/src/views/AboutDevsView.vue
@@ -1,21 +1,20 @@
 <template>
-    <div>
-      <NavBar />
-      <h2>About the App Developers</h2>
-      <p>The DSD Transition Fair App was created by Rachel Buttefield, Weston Schwartz, and Sawyer Heaton from the DSD Catalyst Center.</p>
-      <br>
-      <img src="Rachel.jpg" alt="Rachel Butterfield">
-      <p>Rachel Butterfield</p>
-      <br>
-      <img src="Weston.jpg" alt="Weston Schwartz">
-      <p>Weston Schwartz</p>
-      <br>
-      <img src="Sawyer.jpg" alt="Sawyer Heaton">
-      <p>Sawyer Heaton</p>
-    </div>
-  </template>
+  <div>
+    <NavBar />
+    <h2>About the App Developers</h2>
+    <p>The DSD Transition Fair App was created by Rachel Buttefield, Weston Schwartz, and Sawyer Heaton from the DSD Catalyst Center.</p>
+    <br>
+    <img src="Rachel.jpg" alt="Rachel Butterfield" style="width:500px;height:600px;">
+    <p>Rachel Butterfield</p>
+    <br>
+    <img src="Weston.jpg" alt="Weston Schwartz" style="width:500px;height:600px;">
+    <p>Weston Schwartz</p>
+    <br>
+    <img src="Sawyer.jpg" alt="Sawyer Heaton" style="width:500px;height:600px;">
+    <p>Sawyer Heaton</p>
+  </div>
+</template>
   
-  <script setup>
-  import NavBar from '@/components/NavBar.vue'
-  </script>
-  
+<script setup>
+import NavBar from '@/components/NavBar.vue'
+</script>
