cacf586acc1889691930ef6d55127ef4d33fc91b
MlzzRamdon <26rbutterfield@go.dsdmail.net>
2025-02-24 16:50:07 +0000

    Finished about page

--- Transition Fair/src/views/AboutDevsView.vue
+++ Transition Fair/src/views/AboutDevsView.vue
@@ -4,17 +4,29 @@
     <h2>&nbsp;&nbsp;About the App Developers</h2>
     <p>&nbsp;&nbsp;The DSD Transition Fair App was created by Rachel Buttefield, Weston Schwartz, and Sawyer Heaton from the DSD Catalyst Center.</p>
     <br>
-    <img src="Rachel.jpg" alt="Rachel Butterfield" style="width:500px;height:600px;">
-    <p>Rachel Butterfield</p>
+    <img src="@/assets/Rachel.jpg" alt="Rachel Butterfield" id="peoplePics">
+    <p id="peopleNames">Rachel Butterfield</p>
     <br>
-    <img src="Weston.jpg" alt="Weston Schwartz" style="width:500px;height:600px;">
-    <p>Weston Schwartz</p>
+    <img src="@/assets/Weston.jpg" alt="Weston Schwartz" id="peoplePics">
+    <p id="peopleNames">Weston Schwartz</p>
     <br>
-    <img src="Sawyer.jpg" alt="Sawyer Heaton" style="width:500px;height:600px;">
-    <p>Sawyer Heaton</p>
+    <img src="@/assets/Sawyer.jpg" alt="Sawyer Heaton" id="peoplePics">
+    <p id="peopleNames">Sawyer Heaton</p>
   </div>
 </template>
   
 <script setup>
 import NavBar from '@/components/NavBar.vue'
 </script>
+
+<style scoped>
+#peopleNames {
+  text-align: center;
+}
+#peoplePics {
+  width: 20%;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
