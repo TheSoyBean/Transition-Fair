8043fb2fa0aea52d1c513b1cd5785946d39ea57b
MlzzRamdon <26rbutterfield@go.dsdmail.net>
2025-02-24 09:03:12 -0700

    Made basic outline of about

--- Transition Fair/src/views/AboutDevsView.vue
+++ Transition Fair/src/views/AboutDevsView.vue
@@ -2,7 +2,16 @@
     <div>
       <NavBar />
       <h2>About the App Developers</h2>
-      <p>The DSD Transition Fair App was created by Rachell Buttefield, Weston Schwartz, and Sawyer Heaton from the DSD Catalyst Center.</p>
+      <p>The DSD Transition Fair App was created by Rachel Buttefield, Weston Schwartz, and Sawyer Heaton from the DSD Catalyst Center.</p>
+      <br>
+      <img src="Rachel.jpg" alt="Rachel Butterfield">
+      <p>Rachel Butterfield</p>
+      <br>
+      <img src="Weston.jpg" alt="Weston Schwartz">
+      <p>Weston Schwartz</p>
+      <br>
+      <img src="Sawyer.jpg" alt="Sawyer Heaton">
+      <p>Sawyer Heaton</p>
     </div>
   </template>
   
