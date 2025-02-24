45b951836d536dd0e11f8e4d3dec43d20ab1d5e4
TheSoyBean <edwardsawyerheaton@gmail.com>
2025-02-24 09:42:30 -0700

    Images

--- Transition Fair/src/views/AboutDevsView.vue
+++ Transition Fair/src/views/AboutDevsView.vue
@@ -4,13 +4,13 @@
     <h2>About the App Developers</h2>
     <p>The DSD Transition Fair App was created by Rachel Buttefield, Weston Schwartz, and Sawyer Heaton from the DSD Catalyst Center.</p>
     <br>
-    <img src="Rachel.jpg" alt="Rachel Butterfield" style="width:500px;height:600px;">
+    <img src="@/assets/Rachel.jpg" alt="Rachel Butterfield" style="width:500px;height:600px;">
     <p>Rachel Butterfield</p>
     <br>
-    <img src="Weston.jpg" alt="Weston Schwartz" style="width:500px;height:600px;">
+    <img src="@/assets/Weston.jpg" alt="Weston Schwartz" style="width:500px;height:600px;">
     <p>Weston Schwartz</p>
     <br>
-    <img src="Sawyer.jpg" alt="Sawyer Heaton" style="width:500px;height:600px;">
+    <img src="@/assets/Sawyer.jpg" alt="Sawyer Heaton" style="width:500px;height:600px;">
     <p>Sawyer Heaton</p>
   </div>
 </template>
@@ -18,3 +18,4 @@
 <script setup>
 import NavBar from '@/components/NavBar.vue'
 </script>
+<img src="@/assets/flyer.png" class="modal-content" alt="Fair Flyer" />
