cbf1a81dc3d7aba3d76c39149aa76574e1aec4dc
West253 <164244422+West253@users.noreply.github.com>
2025-02-26 17:42:43 +0000

    broken schedule

--- Transition Fair/src/views/ScheduleView.vue
+++ Transition Fair/src/views/ScheduleView.vue
@@ -2,94 +2,79 @@
   <div>
     <NavBar />
     <table id="schedule-table">
-      <thead>
-        <tr>
-          <th scope="col">Time</th>
-          <th scope="col">Event 1</th>
-          <th scope="col">Event 2</th>
-          <th scope="col">Event 3</th>
-          <th scope="col">Event 4</th>
-          <th scope="col">Event 5</th>
-        </tr>
-      </thead>
       <tbody>
         <tr>
-          <td scope="row">5:00</td>
-          <td colspan="2"><button @click="changeButton($event, 0)">Weber State</button></td>
-          <td><button @click="changeButton($event, 0)">USU</button></td>
-          <td><button @click="changeButton($event, 0)">DSD</button></td>
-          <td><button @click="changeButton($event, 0)">Education and Employment</button></td>
-          <td><button @click="changeButton($event, 0)">Vocational Rehabilitation</button></td>
+          <th scope="row">5:00</th>
+          <td><button @click="changeButton($event)">Vocational Rehabilitation</button></td>
         </tr>
         <tr>
-          <td scope="row">5:30</td>
-          <td><button @click="changeButton($event, 1)">USU</button></td>
-          <td><button @click="changeButton($event, 1)">DSD</button></td>
-          <td><button @click="changeButton($event, 1)">Education and Employment</button></td>
-          <td><button @click="changeButton($event, 1)">Vocational Rehabilitation</button></td>
+          <th scope="row">5:30</th>
+          <td><button @click="changeButton($event)">Utah Parent Center - Transition University</button></td>
         </tr>
         <tr>
-          <td scope="row">6:00</td>
-          <td colspan="2"><button @click="changeButton($event, 2)">Weber State</button></td>
-          <td><button @click="changeButton($event, 2)">USU</button></td>
-          <td><button @click="changeButton($event, 2)">DSD</button></td>
-          <td><button @click="changeButton($event, 2)">Education and Employment</button></td>
-          <td><button @click="changeButton($event, 2)">Vocational Rehabilitation</button></td>
+          <th scope="row">6:00</th>
+          <td><button @click="changeButton($event)">Vista Education Campus</button></td>
         </tr>
         <tr>
-          <td scope="row">6:30</td>
-          <td><button @click="changeButton($event, 3)">USU</button></td>
-          <td><button @click="changeButton($event, 3)">DSD</button></td>
-          <td><button @click="changeButton($event, 3)">Education and Employment</button></td>
-          <td><button @click="changeButton($event, 3)">Vocational Rehabilitation</button></td>
+          <th scope="row">6:30</th>
+          <td><button @click="changeButton($event)">Vocational Rehabilitation</button></td>
         </tr>
         <tr>
-          <td scope="row">7:00</td>
-          <td colspan="2"><button @click="changeButton($event, 4)">Weber State</button></td>
-          <td><button @click="changeButton($event, 4)">USU</button></td>
-          <td><button @click="changeButton($event, 4)">DSD</button></td>
-          <td><button @click="changeButton($event, 4)">Education and Employment</button></td>
-          <td><button @click="changeButton($event, 4)">Vocational Rehabilitation</button></td>
+          <th scope="row">7:00</th>
+          <td><button @click="changeButton($event)">Division of Services for People with Disabilities (DSPD)</button></td>
         </tr>
         <tr>
-          <td scope="row">7:30</td>
-          <td><button @click="changeButton($event, 5)">USU</button></td>
-          <td><button @click="changeButton($event, 5)">DSD</button></td>
-          <td><button @click="changeButton($event, 5)">Education and Employment</button></td>
-          <td><button @click="changeButton($event, 5)">Vocational Rehabilitation</button></td>
+          <th scope="row">7:30</th>
+          <td><button @click="changeButton($event)">Utah Parent Center</button></td>
         </tr>
       </tbody>
     </table>
   </div>
+  <AppFooter />
 </template>
 
 <script setup>
 import NavBar from '@/components/NavBar.vue'
-import { reactive } from 'vue';
+import AppFooter from '@/components/AppFooter.vue'
+import { onMounted } from 'vue'
 
-const selectedButtons = reactive({});
+onMounted(() => {
+    if ('speechSynthesis' in window) {
+        var synthesis = window.speechSynthesis;
+        document.getElementById('tts-button').addEventListener('click', function () {
+            var textElement = document.getElementById('navBar').innerText + ' ' + document.getElementById('schedule-table').innerText;
+            var text = textElement.innerText;
+            var utterance = new SpeechSynthesisUtterance(text);
+            utterance.lang = 'en-US';
+            utterance.pitch = 1;
+            utterance.rate = 1;
+            utterance.volume = 1;
+            window.speechSynthesis.speak(utterance);
+        });
 
-const changeButton = (event, rowIndex) => {
-  // Deselect previous button in the same row
-  if (selectedButtons[rowIndex]) {
-    selectedButtons[rowIndex].style.backgroundColor = "";
-  }
-
-  // Select the clicked button
-  event.target.style.backgroundColor = "#B2BEB5";
-  selectedButtons[rowIndex] = event.target;
-};
+        window.addEventListener('beforeunload', function () {
+            if (synthesis.speaking) {
+                synthesis.cancel();
+            }
+        });
+    }
+    else {
+        console.error('TTS not supported');
+    }
+});
 </script>
 
 <style scoped>
 #schedule-table {
-  display: flex;
-  justify-content: center;
-  align-items: flex-start;
-  height: 100vh;
-  padding-top: 20px; /* Adjust this value as needed */
+  margin: 20px auto;
+  width: 80%;
+  border-collapse: collapse;
 }
 .td, .th {
   border-spacing: 10px;
+}
+
+button.selected {
+  background-color: #B2BEB5;
 }
 </style>
