3df7f0d7ecfd62ce9f65a317da18aef892886f21
West253 <164244422+West253@users.noreply.github.com>
2025-02-26 16:38:34 +0000

    Implemented new table map

--- Transition Fair/src/views/MapView.vue
+++ Transition Fair/src/views/MapView.vue
@@ -3,16 +3,22 @@
     <NavBar />
     <button id="tts-button" class="tts-button"></button>
     <div class="map">
-      <img src = "@/assets/map.png"></img>
+      <img src = '@/assets/map.png' class="flyer" alt="Fair Flyer" @click="showModal = true"/>
+    <div v-if="showModal" class="modal" @click.self="showModal = false">
+      <img src="@/assets/map.png" class="modal-content" alt="Fair Flyer"/>
+    </div>
     </div>
   </div>
   <AppFooter />
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue'
 import NavBar from '@/components/NavBar.vue'
 import AppFooter from '@/components/AppFooter.vue'
-import { onMounted } from 'vue'
+
+const showModal = ref(false)
+
 onMounted(() => {
   if ('speechSynthesis' in window) {
     var synthesis = window.speechSynthesis;
@@ -40,12 +46,28 @@ onMounted(() => {
 </script>
 
 <style scoped>
-.map {
+.flyer {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  height: 85vh;
+}
+
+.modal {
   display: flex;
   justify-content: center;
-  align-items: flex-start;
-  height: 100vh;
-  padding-top: 20px;
-  /* Adjust this value as needed */
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.8);
+  z-index: 1000;
+}
+
+.modal-content {
+  max-width: 90%;
+  max-height: 90%;
 }
 </style>
